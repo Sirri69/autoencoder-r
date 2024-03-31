884319e401f7b7a6594a5fff50173e1bc001d9ab
Sirri69 <pranav2278@gmail.com>
2024-03-31 21:13:50 +0530

    Moved layers to `modeling_llama_c.py` and overrode RMSNorm

--- compressed_llama/modeling_llama_c.py
+++ compressed_llama/modeling_llama_c.py
@@ -262,6 +262,28 @@ class LlamaMLP(nn.Module):
         return down_proj
 
 
+class CustomLLamaMLP(nn.Module):
+    def __init__(self, original_mlp):
+        super(CustomLLamaMLP, self).__init__()
+        # Initialize the layers using the existing MLP layers
+        self.gate_proj = original_mlp.gate_proj
+        self.up_proj = original_mlp.up_proj
+        # Copying down_proj from the original MLP
+        self.down_proj = original_mlp.down_proj
+        # Inserting your new layer here, scaling down to 2048
+        self.downscale_proj = nn.Linear(4096, 2048)
+        # Keeping the original activation function
+        self.act_fn = original_mlp.act_fn
+
+    def forward(self, x):
+        """
+        No tensor parallelism for now. Simple forward pass.
+        """
+        down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+        downscale_proj = self.act_fn(self.downscale_proj(down_proj))
+        return downscale_proj
+
+
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     """
     This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
@@ -772,6 +794,94 @@ class LlamaDecoderLayer(nn.Module):
         residual = hidden_states
         hidden_states = self.post_attention_layernorm(hidden_states)
         hidden_states = self.mlp(hidden_states)
+        hidden_states = residual + hidden_states
+
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (self_attn_weights,)
+
+        if use_cache:
+            outputs += (present_key_value,)
+
+        return outputs
+
+
+class CustomLlamaDecoderLayer(nn.Module):
+    """
+    This decoder layer is a custom implementation of the LlamaDecoderLayer. 
+    The main difference is that it uses the CustomLLamaMLP instead of the LlamaMLP.
+    Additionally, it uses a residual_mlp layer to compress the residuals too.
+    """
+
+    def __init__(self, config: LlamaConfig, layer_idx: int):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+
+        self.self_attn = LlamaAttention()
+
+        self.mlp = CustomLLamaMLP(LlamaMLP(config))
+        self.residual_mlp = CustomLLamaMLP(LlamaMLP(config)) # new MLP to compress the residuals
+        self.input_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        output_attentions: Optional[bool] = False,
+        use_cache: Optional[bool] = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        **kwargs,
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+        """
+        Args:
+            hidden_states (`torch.FloatTensor`): input to the layer of shape `(batch, seq_len, embed_dim)`
+            attention_mask (`torch.FloatTensor`, *optional*):
+                attention mask of size `(batch_size, sequence_length)` if flash attention is used or `(batch_size, 1,
+                query_sequence_length, key_sequence_length)` if default attention is used.
+            output_attentions (`bool`, *optional*):
+                Whether or not to return the attentions tensors of all attention layers. See `attentions` under
+                returned tensors for more detail.
+            use_cache (`bool`, *optional*):
+                If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding
+                (see `past_key_values`).
+            past_key_value (`Tuple(torch.FloatTensor)`, *optional*): cached past key and value projection states
+        """
+        if "padding_mask" in kwargs:
+            pass
+            # warnings.warn(
+            #     "Passing `padding_mask` is deprecated and will be removed in v4.37. Please make sure use `attention_mask` instead.`"
+            # )
+
+        residual = hidden_states
+
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # Self Attention
+        hidden_states, self_attn_weights, present_key_value = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+            cache_position=cache_position,
+            **kwargs,
+        )
+
+        hidden_states = residual + hidden_states
+
+        # Fully Connected
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states = self.mlp(hidden_states)
+
+        # scale down the residuals to the shape of the hidden_states
+        residual = self.residual_mlp(residual)
+        
         hidden_states = residual + hidden_states
 
         outputs = (hidden_states,)
